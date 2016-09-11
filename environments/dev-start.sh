adb27cfa56567f29a38ffbe920b491ee34275bf9
Gevorg Soghomonian <gevorg@Gevorgs-MacBook-Pro.local>
2016-09-11 19:46:46 +0100

    added startup scripts for the spark environment

--- environments/dev-start.sh
+++ environments/dev-start.sh
@@ -1,1 +1,4 @@
 #!/bin/bash
+
+# Link to my emacs configs
+ln -s ~/repos/.emacs.d ~/
---
+++ environments/spark-env-config.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+BASE_DIR=$(dirname $0)
+source $BASE_DIR/dev-start.sh
+
+# Set JDK path
+echo "export PATH=$PATH:/usr/local/java/bin" >> ~/.bashrc
+
+# Set Apache Spark path
+echo "export PATH=$PATH:/usr/local/spark/bin" >> ~/.bashrc
+
+source ~/.bashrc
