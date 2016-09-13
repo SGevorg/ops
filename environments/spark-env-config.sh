524dbd0c6ce3d5cbf5cd6ab0ca3cdd27cdfabe59
Gevorg Soghomonian <gevorg.soghomonyan.gs@gmail.com>
2016-09-13 08:45:20 +0100

    fix the bashrc inclusions and add data as a shared folder

--- environments/spark-env-config.sh
+++ environments/spark-env-config.sh
@@ -4,9 +4,9 @@ BASE_DIR=$(dirname $0)
 source $BASE_DIR/dev-start.sh
 
 # Set JDK path
-echo "export PATH=$PATH:/usr/local/java/bin" >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/java/bin' >> ~/.bashrc
 
 # Set Apache Spark path
-echo "export PATH=$PATH:/usr/local/spark/bin" >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/spark/bin' >> ~/.bashrc
 
 source ~/.bashrc
