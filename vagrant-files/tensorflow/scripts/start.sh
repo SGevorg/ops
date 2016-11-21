2500b7ff9cab15a3c80db4c540d43f1ecd105688
Gevorg Soghomonian <gevorg.soghomonyan.gs@gmail.com>
2016-11-21 08:40:44 +0000

    tensorflow box added

---
+++ vagrant-files/tensorflow/scripts/start.sh
@@ -0,0 +1,1 @@
+docker run -it --name ipython -p 8888:8888 -v ~/repos/ops/vagrant-files/tensorflow/data:/data -v ~/repos/ops/vagrant-files/tensorflow/notebooks:/notebooks gcr.io/tensorflow/tensorflow
